9e997c536dbbcc4d28a3d2c3d38fcf7072a9a342
Daniel Granger <dg1234uk@me.com>
2024-05-28 14:58:11 +0100

    FZF added

--- lua/dg1234uk/plugins/telescope.lua
+++ lua/dg1234uk/plugins/telescope.lua
@@ -1,18 +1,25 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
-    require("telescope").setup({})
+    local telescope = require("telescope")
+    telescope.setup({})
+    telescope.load_extension("fzf")
 
     -- TODO: Add more dependcies and plugins
 
     local builtin = require("telescope.builtin")
 
     -- TODO: Add more keymaps
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles in CWD" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind [g]rep - Search for a string in CWD" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]uffers - Lists open buffers in current nvim instance" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp tags" })
+    vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "[f]ind [c]ommands - Lists available plugin/user commands" })
   end,
 }
